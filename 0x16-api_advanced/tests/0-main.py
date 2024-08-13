6bf06bb70b361154a3bcf309a234ff3daf9110f5
soufianetangi <soufianetangi2@gmail.com>
2024-08-13 16:53:11 -0700

    Updated program

--- 0x16-api_advanced/tests/0-main.py
+++ 0x16-api_advanced/tests/0-main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""
+0-main
+"""
 import sys
 
-
 if __name__ == '__main__':
-    number_of_subscribers = __import__('subs_0').number_of_subscribers
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
