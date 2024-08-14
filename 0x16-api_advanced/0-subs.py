871c945674ac38f60cff3ce57c99b020e02c1e1f
soufianetangi <soufianetangi2@gmail.com>
2024-08-14 07:56:52 -0700

    Updated program

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,16 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"}
-    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
-    
-    data = response.json().get("data", {})
-    
-    return data.get("subscribers", 0)
+    results = response.json().get("data")
+    return results.get("subscribers")
 
