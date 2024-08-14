7bb2ab44a86a4b285289dc551afd9d355244ed42
soufianetangi <soufianetangi2@gmail.com>
2024-08-14 09:24:20 -0700

    Updated program

--- 0x16-api_advanced/tests/0-main.py
+++ 0x16-api_advanced/tests/0-main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+0-main
+"""
 import sys
+import os
 
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == '__main__':
-    number_of_subscribers = __import__('src.0-subs').number_of_subscribers
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
