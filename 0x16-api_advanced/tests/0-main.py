cde8ca7f5390dcea27446f26f09559f45e6135c1
soufianetangi <soufianetangi2@gmail.com>
2024-08-14 09:29:05 -0700

    Updated program

--- 0x16-api_advanced/tests/0-main.py
+++ 0x16-api_advanced/tests/0-main.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""
-0-main
-"""
 import sys
-import os
 
-# Add the parent directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == '__main__':
     number_of_subscribers = __import__('0-subs').number_of_subscribers
