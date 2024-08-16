aa6c59b130801c636df9adcd15c5ec45bbef0d27
soufianetangi <soufianetangi2@gmail.com>
2024-08-16 18:45:59 +0000

    Updated program

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,29 @@
-# automated puppet fix (to find out why Apache is returning a 500 error)
+# This Puppet manifest automates the fix for Apache 500 Internal Server Error
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+# Ensure the Apache service is running
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
+
+# Ensure necessary packages are installed
+package { ['libapache2-mod-php5', 'php5', 'php5-mysql']:
+  ensure => installed,
+}
+
+# Fix file permissions for Apache
+file { '/var/www/html/wp-content':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  recurse => true,
+}
+
+# Restart Apache service to apply changes
+exec { 'restart_apache':
+  command     => '/etc/init.d/apache2 restart',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-content'],
+}
+
