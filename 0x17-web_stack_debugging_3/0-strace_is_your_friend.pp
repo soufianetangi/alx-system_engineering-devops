2dede587776759838a29ff3eb00272c0e4e4422b
soufianetangi <soufianetangi2@gmail.com>
2024-08-17 16:02:57 +0000

    Updaed program

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,28 @@
-# automated puppet fix (to find out why Apache is returning a 500 error)
+# This Puppet manifest fixes the issue causing Apache to return a 500 Internal Server Error.
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+# Ensure the required package is installed
+package { 'wordpress':
+  ensure => installed,
+}
+
+# Ensure that the Apache service is running
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
+
+# Ensure correct permissions for WordPress files
+file { '/var/www/html/wp-content':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+}
+
+# Run the WordPress upgrade script to fix any potential issues
+exec { 'fix-wordpress-site':
+  command => '/usr/bin/php /var/www/html/wp-admin/upgrade.php',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/php /var/www/html/wp-admin/upgrade.php --check',
 }
 
