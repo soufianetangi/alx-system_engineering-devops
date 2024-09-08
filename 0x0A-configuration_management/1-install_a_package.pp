c48ef9129aab4322b551106806c6f812c8d2a83d
soufianetangi <soufianetangi2@gmail.com>
2024-09-08 11:45:59 -0700

    Updated program

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Puppet manifest to install Flask 2.1.0 and Werkzeug 2.0.3
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask 2.1.0
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Install Werkzeug 2.0.3 (compatible with Flask 2.1.0)
+package { 'Werkzeug':
+  ensure   => '2.0.3',
+  provider => 'pip3',
 }
